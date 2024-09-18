afbd5d32f66712815e92502c946d4147a6769edb
Maxouwe <100154435+Maxouwe@users.noreply.github.com>
2024-09-18 20:40:09 +0200

    basis functionaliteit

--- SIR-simulation/SIR-simulation/DiffSystem.cs
+++ SIR-simulation/SIR-simulation/DiffSystem.cs
@@ -15,7 +15,7 @@ namespace SIR_simulation
         //population size
         public int N;
         //holds the current s(t), i(t), r(t) values
-        List<float> currentValues;
+        public List<float> currentValues;
 
         //supply capital S(0), I(0), R(0) values and population size
         public DiffSystem(float b, float k, float SStart, float IStart, float RStart, int N)
@@ -38,33 +38,42 @@ namespace SIR_simulation
         public void calcStep()
         {
             //we have the s(t), i(t) and r(t) values calculated from the previous step
+            float st = currentValues[0];
+            float it = currentValues[1];
+            float rt = currentValues[2];
 
             //insert the values into the equations
+            float dSdt = calcdsdt(st, it, b);
+            float dIdt = calcdidt(st, it, b, k);
+            float dRdt = calcdrdt(it, k);
+
             //by doing this we obtain the slope of each function
             //we just add the slope value to the current s(t), i(t), r(t) values to get s(t+1), i(t+1), r(t+1)
-
             //copy the results to this.currentValues
+            currentValues[0] = st + dSdt;
+            currentValues[1] = it + dIdt;
+            currentValues[2] = rt + dRdt;
+
             //we can access this.currentValues from outside every step to create graphs etc.
-            throw new NotImplementedException();
         }
 
 
         //calculates ds/dt based on given input s(t), i(t) and b
-        private float calcStepdSdt(float s, float i, float b)
+        private float calcdsdt(float s, float i, float b)
         {
-            throw new NotImplementedException ();
+            return (-b) * s * i;
         }
 
         //calculates di/dt based on given input s(t), i(t), b and k
-        private float calcStepdIdt(float s, float i, float b, float k)
+        private float calcdidt(float s, float i, float b, float k)
         {
-            throw new NotImplementedException();
+            return b * s * i - k * i;
         }
 
         //calculates dr/dt based on given input i(t) and k
-        private float calcStepdRdt(float i, float k)
+        private float calcdrdt(float i, float k)
         {
-            throw new NotImplementedException();
+            return k * i;
         }
     }
 }
--- SIR-simulation/SIR-simulation/Program.cs
+++ SIR-simulation/SIR-simulation/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("you want some? y/n");
+            while (Console.ReadLine() == "y")
+            {
+                Console.WriteLine("how many contacts per day");
+                float b = float.Parse(Console.ReadLine());
+                Console.WriteLine("what is the recovery rate");
+                float k = float.Parse(Console.ReadLine());
+                Console.WriteLine("how big is the population");
+                int N = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("How many susceptibles at t=0");
+                float S0 = float.Parse(Console.ReadLine());
+                Console.WriteLine("How many infected at t=0");
+                float I0 = float.Parse(Console.ReadLine());
+                Console.WriteLine("How many recovered at t=0");
+                float R0 = float.Parse(Console.ReadLine());
+
+                DiffSystem sys = new DiffSystem(b, k, S0, I0, R0, N);
+
+                for (int i = 0; i < 150; i++)
+                {
+                    sys.calcStep();
+
+                    //output every 10th step
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine(sys.currentValues[0] + " " + sys.currentValues[1] + " " + sys.currentValues[2]);
+                    }
+                }
+                Console.WriteLine("want more y/n?");
+            }
         }
     }
 }
